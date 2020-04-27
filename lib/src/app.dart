593a744252deb0e7ffa3065211cfd39d7208c717
pete <ptrmutie@gmail.com>
2020-04-27 16:17:54 +0300

    created the main file for the app

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+
+  Widget build(context){
+    return MaterialApp(
+      title: 'Log me in',
+      home: Scaffold(
+        body: Text('login form goes here'),
+      )
+
+    );
+  } 
+}
